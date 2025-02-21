c129ee8db624ac6778501288a5bee2e66ee3bb9b
HABIBA HASSAN GUYO <guyohabibahassan@gmail.com>
2025-02-21 15:23:49 +0300

    added completing labs for file io lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
-    pass
+    """Creates a .txt file and writes content to it."""
+    with open(str(file_name) + ".txt", "w") as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    """Appends content to an existing .txt file."""
+    with open(str(file_name) + ".txt", "a") as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    """Reads and returns the content of a .txt file."""
+    with open(str(file_name) + ".txt", "r") as file:
+        return file.read()
